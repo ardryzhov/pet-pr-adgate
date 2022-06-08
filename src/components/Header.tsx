572595c901c37b73eb21fad1dff46f9124eddc44
Artem <ard.ryzhov@gmail.com>
2022-06-08 19:57:52 +0300

    create header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,40 @@
 import React from 'react'
 
+import { useAppSelector } from '../hooks'
+
 import { Link } from 'react-router-dom'
-import { AppBar, Container, Typography, Box } from '@mui/material'
+import { AppBar, Container } from '@mui/material'
 import { styled } from '@mui/material/styles'
 
-const Header = () => {
+const Header: React.FC = () => {
+	const login = useAppSelector((state) => state.auth)
+
+	const HeaderLink = styled(Link)(({ theme }) => ({
+		fontSize: '2rem',
+		margin: '1rem 0',
+		color: '#e3c9a6',
+		[theme.breakpoints.down('sm')]: {
+			fontSize: '1.2rem',
+		},
+	}))
 	return (
-		<>
-			<AppBar>
-				<Container
-					sx={{
-						display: 'flex',
-						flexDirection: 'row',
-						justifyContent: 'space-around',
-					}}
-				></Container>
-			</AppBar>
-		</>
+		<AppBar position='static' color='secondary'>
+			<Container
+				sx={{
+					display: 'flex',
+					flexDirection: 'row',
+					justifyContent: 'space-around',
+				}}
+			>
+				<HeaderLink to='/'>AdGate</HeaderLink>
+				<HeaderLink to='/news'>News</HeaderLink>
+				{login ? (
+					<HeaderLink to='/profile'>Profile</HeaderLink>
+				) : (
+					<HeaderLink to='/login'>Login</HeaderLink>
+				)}
+			</Container>
+		</AppBar>
 	)
 }
 
